eab6fe88951d04da953c2abdf870553b6dc2f8e1
MasonAmadeus <71096260+masonamadeus@users.noreply.github.com>
2020-12-10 19:03:54 -0500

    Cleaning Up

--- sprayerv2/launchfacespray.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /home/pi/Desktop/sprayerv2
-export FLASK_APP=facespray.py
-sleep 1
-python3 -m flask run
--- sprayerv2/launchtunnel.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /home/pi/Desktop/sprayerv2
-./ngrok http 5000
